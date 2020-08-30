2a954395485bd8636578b2e9bc8d8823daad12f4
Kazuki Ota <kaota@microsoft.com>
2020-08-30 23:56:37 +0900

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿
+using Livet;
+using System;
+
+namespace TestTransitionMessage.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+	{
+		public SubWindowViewModel SubWindowViewModel { get; } = new SubWindowViewModel();
+
+        public MainWindowViewModel()
+        {
+			CompositeDisposable.Add(SubWindowViewModel);
+        }
+
+		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+		public void Initialize()
+		{
+		}
+    }
+}
---
+++ ViewModels/SubWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Livet;
+using System.Diagnostics;
+
+namespace TestTransitionMessage.ViewModels
+{
+	public class SubWindowViewModel : ViewModel
+	{
+		public void Initialize()
+		{
+			// 2 回目のウィンドウオープンで 'Cannot access a disposed object' エラーが発生する
+			// Visual Studio 2019 + Livet v3.2.1
+			Debug.WriteLine("Initialize() CompositeDisposable.Count: " + CompositeDisposable.Count);
+		}
+	}
+}
